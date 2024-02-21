3ee686cd6166d2e0299b0505be3332294b4c7147
admin_signin <siem.choub@student.passerellesnumeriques.org>
2024-02-22 01:30:09 +0700

    form

--- authentication_router.php
+++ authentication_router.php
@@ -6,10 +6,12 @@ $routes = [
     '/signup' => 'controllers/signup/signup.controller.php',
     '/product' => 'controllers/product/product.controller.php',
     '/manager' => 'controllers/manager/manager.controller.php',
+    '/admin' => 'controllers/admin/admin_home.controller.php'
+
 
     // '/trainer-review' => 'controllers/reviews/review.controller.php',
     // '/trainer-classroom' => 'controllers/classroom/classroom.controller.php',
-   
+
 
 
 ];
@@ -17,7 +19,7 @@ $routes = [
 if (array_key_exists($uri, $routes)) {
     $page = $routes[$uri];
 } else {
-   http_response_code(404);
-   $page = 'views/errors/404.php';
+    http_response_code(404);
+    $page = 'views/errors/404.php';
 }
 require $page;
