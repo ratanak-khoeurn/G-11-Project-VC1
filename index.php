0bf855bd367c8315e504b483aa892c645084bf43
MeyHeang <sokheang.men@student.passerellesnumeriques.org>
2024-02-24 14:24:36 +0700

    [feat] #11

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require 'utils/url.php';
 require 'database/database.php';
-if ( urlIs('/category') || urlIs('/add_admin') || urlIs('/restaurant_admin') || urlIs('/product_admin')|| urlIs('/manager_admin') || urlIs('/customer_admin') || urlIs('/deliverer_admin')|| urlIs('/admin_home')){ 
+if ( urlIs('/category') || urlIs('/add_admin') || urlIs('/restaurant_admin') || urlIs('/product_admin')|| urlIs('/manager_admin') || urlIs('/customer_admin') || urlIs('/deliverer_admin')|| urlIs('/admin_home')|| urlIs('/update_category')){ 
     require "admin_router.php";
 
 } else if (urlIs('/signin') || urlIs('/signup')|| urlIs("/manager")|| urlIs("/admin")) {
