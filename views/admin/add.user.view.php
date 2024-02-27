1200802b9b52844f25b6ebcbb5d3c1582cc0680c
Siem-Choub <siem.choub@student.passerellesnumeriques.org>
2024-02-27 20:48:30 +0700

    [feat]

--- views/admin/add.user.view.php
+++ views/admin/add.user.view.php
@@ -1,33 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delivery Management</title>
     <link rel="stylesheet" href="../../vendor/css/add_user.css">
 </head>
+
 <body>
     <div class="container">
         <h2 style="margin-top: 20px;">Delivery Management System</h2>
         <div id="my_modal" class="modal">
             <div class="modal-content">
-                <span class="close">&times;</span>
                 <form action="#" method="post">
-                    <div class="form-group">
-                        <label for="firstname">First Name</label>
-                        <input type="text" id="firstname" name="firstname" placeholder="Enter your first name">
+                    <div class="names">
+                        <div class="form-group-name">
+                            <label for="firstname">First Name</label>
+                            <input type="text" id="firstname" name="firstname" placeholder="Enter your first name">
+                        </div>
+                        <div class="form-group-name">
+                            <label for="lastname">Last Name</label>
+                            <input type="text" id="lastname" name="lastname" placeholder="Enter your last name">
+                        </div>
+                    </div>
+                    <div class="names">
+                        <div class="form-group-name">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" name="email" placeholder="Enter your email">
+                        </div>
+                        <div class="form-group-name">
+                            <label for="password">Password</label>
+                            <input type="password" id="password" name="password" placeholder="Enter your password">
+                        </div>
+
                     </div>
                     <div class="form-group">
-                        <label for="lastname">Last Name</label>
-                        <input type="text" id="lastname" name="lastname" placeholder="Enter your last name">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter your email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter your password">
+                        <label for="restaurant_name">Restaurant Name</label>
+                        <select class="form-control" id="restaurant_name" name="restaurant_name" required>
+                            <option value="">Select a restaurant</option>
+                            <option value="restaurant1">Restaurant 1</option>
+                            <option value="restaurant2">Restaurant 2</option>
+                            <option value="restaurant3">Restaurant 3</option>
+                            <!-- Add more options as needed -->
+                        </select>
                     </div>
                     <button class="submit" type="submit">Submit</button>
                 </form>
@@ -35,4 +51,5 @@
         </div>
     </div>
 </body>
+
 </html>
