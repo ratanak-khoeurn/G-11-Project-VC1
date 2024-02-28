2b9e43f1097a02da20ab02c839fb97862e13ec66
sreyluchly <sreyluch.ly@student.passerellesnumeriques.org>
2024-02-28 20:12:23 +0700

    [feat] update deliverer #FPG-49

---
+++ views/deliverer/list_customer.php
@@ -0,0 +1,61 @@
+<link rel="stylesheet" href="../../../vendor/css/deliverer_page.css">
+<div class="navbar">
+    <nav>
+        <div class="left">
+            <a href=""><img src="../../assets/images/logo_web.png" alt="" style="width: 70px;height:70px;border-radius:50%">
+            </a>
+            <h2>Deliverer</h2>
+        </div>
+        <div class="right">
+            <span>Name</span>
+            <span><a href=""><img src="../../../assets/images/avatar/no-profile-pic-icon-11.jpg" alt="" class="profile"></a></span>
+        </div>
+    </nav>
+</div>
+
+<div class="container">
+    <div class="sidebar">
+        <div class="list">
+            <ul>
+                <li>
+                    <a href="#">Customer</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="list_deliverer.php">Deliverer</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <main>
+        <h1>Customer Orders</h1>
+        <div class="card">
+            <div class="card_hearder">
+                <h2>Food Good</h2>
+            </div>
+            <div class="card_body">
+                <img src="../../../assets/images/trending7.png" alt="" class="card_pic">
+            </div>
+            <div class="card_footer">
+                <div class="footer_left">
+                    <h4>Brohok</h4>
+                    <p>Address:Pursat</p>
+                    <p>Email:ps@gmail.com</p>
+                    <p>Number Phone: 0888866</p>
+                    <p>Pick up: yes</p>
+                </div>
+            </div>
+            <div class="btn">
+                <button class="btn1">Accept</button>
+                <button class="btn2">Cencele</button>
+            </div>
+
+        </div>
+    </main>
+
+    <footer></footer>
+</div>
+
+</div>
+</div>
---
+++ views/deliverer/list_deliverer.php
@@ -0,0 +1,57 @@
+<link rel="stylesheet" href="../../../vendor/css/deliverer_page.css">
+<div class="navbar">
+    <nav>
+        <div class="left">
+            <a href=""><img src="../../assets/images/logo_web.png" alt="" style="width: 70px;height:70px;border-radius:50%">
+            </a>
+            <h2>Deliverer</h2>
+        </div>
+        <div class="right">
+            <span>Name</span>
+            <span><a href=""><img src="../../../assets/images/avatar/no-profile-pic-icon-11.jpg" alt="" class="profile"></a></span>
+        </div>
+    </nav>
+
+</div>
+
+<div class="container">
+    <div class="sidebar">
+        <div class="list">
+            <ul>
+                <li>
+                    <a href="list_customer.php">Customer</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">Deliverer</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <main>
+        <h1>List Deliverer</h1>
+        <div class="card_deliverer">
+            <div class="card_header_deliverer">
+                <img src="../../../assets/images/trending7.png" alt="" class="card_deliverer_pic">
+            </div>
+            <div class="card_body_deliverer">
+                <h3>KaKa</h3>
+            </div>
+            <div class="card_footer_deliverer">
+                <div class="footer_left_deliverer">
+                    <p>Address:Pursat</p>
+                    <p>Email:ps@gmail.com</p>
+                    <p>Number Phone: 0888866</p>
+                    <p>Age:33</p>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    <footer></footer>
+</div>
+
+</div>
+</div>
