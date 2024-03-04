cc4827cf9de4f60494b36492fc709731fda87467
Siem-Choub <siem.choub@student.passerellesnumeriques.org>
2024-03-04 20:30:03 +0700

    [imp] PFG-57

--- controllers/logout/logout.controller.php
+++ controllers/logout/logout.controller.php
@@ -1,3 +1,10 @@
-<?
+<?php
+session_start(); // Start the session
+
+// Destroy the session
 session_destroy();
-header('location:logout');
+
+// Redirect the user to the homepage
+header('Location: /');
+exit(); // Terminate script execution after redirect
+?>
