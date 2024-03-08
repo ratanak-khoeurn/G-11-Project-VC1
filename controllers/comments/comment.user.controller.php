8d81d317dc2d00b39bcf19163e936943733b0998
Siem-Choub <siem.choub@student.passerellesnumeriques.org>
2024-03-08 12:20:05 +0700

    [feat] already add feedback #FPG-64

--- controllers/comments/comment.user.controller.php
+++ controllers/comments/comment.user.controller.php
@@ -1,6 +1,10 @@
 <?php
-// require "../../models/comments.model.php";
+session_start();
+require  "../../database/database.php";
+require "../../models/comments/comments.model.php";
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $comment = $_POST['comment'];
-    echo $comment;
+    store_comment($comment,$_SESSION['user']['first_name'],$_SESSION['user']['picture']);
+    echo $_SESSION['user']['first_name'];
+    
 }
