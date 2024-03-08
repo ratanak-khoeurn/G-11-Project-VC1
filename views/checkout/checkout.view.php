7eb5b66822dfce6d7eca4d6f2b17a629bbe7ca5e
sreyluchly <sreyluch.ly@student.passerellesnumeriques.org>
2024-03-08 11:50:37 +0700

    [feat] update code choose the way on the pay #FPG-70

--- views/checkout/checkout.view.php
+++ views/checkout/checkout.view.php
@@ -1,15 +1,16 @@
-<div class="checkout" style="width:100%; display:flex; justify-content:space-evently;">
+<div class="checkout" style="width:100%; display:flex;justify-content:center">
     <div class="checkout-left">
-        <div class="card" style=" padding:30px; margin:30px;">
+        <!-- <div class="container"> -->
+        <div class="card" style=" padding:10px; margin:30px 30px 30px 0;width:100%">
             <div class="row">
                 <div class="col-md-4">
-                    <img src="assets/images/FOOD.jpg" class="card-img" alt="...">
+                    <img src="assets/images/FOOD.jpg" class="card-img" alt="" width="20px" >
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <div class="card-1" style="display:flex; justify-content:space-between">
                             <h5 class="me-3">Khmer food</h5>
-                            <input type="number" style="width:50px;">
+                            <input type="number" style="width:15px;">
                             <p>100$</p>
                         </div>
                         <p class="me-3">Size:Size</p>
@@ -22,41 +23,12 @@
                     </div>
                 </div>
             </div>
-
-
+            
         </div>
+        <div class="row">kjdjfd</div>​
+        <!-- </div> -->
 
-        <div class="card mb-6" style=" padding:30px; margin:30px;">
-            <p>Delivery</p>
-        </div>
-
-        <div class="card mb-6" style=" display:flex; flex-direction:row; gap: 10px; padding:30px; margin:30px;">
-            <img src="assets/images/checkout/ABA.jpg" class="card-img" alt="" style="width:30px">
-            <img src="assets/images/checkout/AC.png" class="card-img" alt="" style="width:30px">
-            <img src="assets/images/checkout/WING.png" class="card-img" alt="" style="width:30px">
-        </div>
-    </div>
-
-    <div class="checkout-right">
-        <div class="card" style="width:90%; padding:30px; margin:30px;">
-            <h3>Total</h3>
-            <div class="subtotal"  style=" display:flex; flex-direction:row;  justify-content:space-between;">
-                <p>Subtotal</p>
-                <p>£219.99</p>
-            </div>
-            <div class = "delivery" style=" display:flex; flex-direction:row;  justify-content:space-between;">
-                <p>Delivery</p>
-                <p>Free</p>
-            </div>
-            <div class="total" style=" display:flex; flex-direction:row; justify-content:space-between;">
-                <p>Total (VAT included)</p>
-                <p>£219.99</p>
-            </div>
-            <p class="text-muted">Items placed in this bag are not reserved.</p>
-            <a href="#" class="btn btn-dark" style="background:#E21B70; border:none;">GO TO CHECKOUT</a>
-        </div>
     </div>
 </div>
 
 </div>
-
